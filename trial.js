5d792c8d35c8277a6940eeaf891271ba5bf29233
Jovz19200 <gisubizo.jovan@gmail.com>
2022-08-09 17:52:22 +0200

    second commit

--- trial.js
+++ trial.js
@@ -1,10 +1,11 @@
 const prov_Districts = {
-   "Kigali" : [
+   "Kigali" : ["Choose District",
               "Gasabo", 
               "Kicukiro",
               "Nyarugenge"
    ],
    "South" : [
+             "Choose District",
              "Kamonyi",
              "Muhanga",
              "Ruhango",
@@ -14,7 +15,7 @@ const prov_Districts = {
              "Nyamagabe",
              "Nyaruguru"
    ],
-   "West": [
+   "West": ["Choose District",
             "Rusizi",
             "Nyamasheke",
             "Rutsiro",
@@ -24,31 +25,78 @@ const prov_Districts = {
             "Ngororero"
    ],
    "East" : [
-            "Rwamagana",
+            "Choose District",
+            "Nyagatare",
             "Bugesera",
             "Ngoma",
             "Kirehe",
             "Kayonza",
             "Gatsibo",
-            "Nyagatare"
+            "Rwamagana"
    ],
    "North": [
+            "Choose District",
             "Rulindo",
             "Gakenke",
             "Musanze",
             "Burera",
             "Gicumbi"
-   ]
+   ],
+   "drc": ["+243"],
+   "sudan": ["+249"],
+   "rwanda": ["+250"],
+   "kenya": ["+254"],
+   "tanzania": ["+255"],
+   "uganda": ["+256"],
+   "burundi":["+257"]
    
 };
 
 let listDistrict = document.getElementById("select_2");
 let listProvince = document.getElementById("select_1");
+let listOfCountries = document.getElementById("countries");
+let listOfCode = document.getElementById("chooseCode");
 let assignDistrict = () => {
+    
+    
     listDistrict.innerHTML = "";
     let province = listProvince.value;
+
 
     for (var i = 0 ; i <  prov_Districts[province].length; i++){
          listDistrict.innerHTML += `<option value="">${prov_Districts[province][i]}</option>`;
     }
+    
 };
+
+let displayCode = () => {
+    listOfCode.innerHTML="";
+    let Country = listOfCountries.value;
+
+    for (var j= 0 ; j<prov_Districts[Country].length; j++){
+        listOfCode.innerHTML += `<option value="">${prov_Districts[Country][j]} </option>`
+    }
+}
+ let n1 = document.getElementById("n1");
+ let n2 = document.getElementById("n2");
+ 
+ /*var validate = () => {
+    if (n1.value != "" && n1.value != ""){
+    if (n1.value == n2.value){
+        return true;
+    }}
+    alert("Your Password did not match");
+     return false;
+     
+ }*/
+ let mail = document.getElementById("mail");
+ let email = mail.value;
+ function validate(email){
+    
+    let n= email.indexOf("@gmail.com");
+    if (n >=1){
+        return true;
+    }
+    alert("Check on your Gmail");
+    
+ }
